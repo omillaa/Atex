46b994806fcd69a21636d65988703e3fb8d5cb27
laripsilva25 <117595456+laripsilva25@users.noreply.github.com>
2023-11-28 15:36:03 -0300

    Alteração prompt

--- script.js
+++ script.js
@@ -23,50 +23,34 @@ function generatePlanoDeAula(pergunta, idioma) {
     let plano = '';
 
     if (idioma === 'ingles') {
-        plano = `
-        Generate a lesson plan for ${pergunta}.
-        1. Introduction: - Describe the importance of the ${pergunta} topic in everyday life or within the discipline context.
-        2. Objectives: - Define the learning objectives, highlighting what students should understand or be able to do by the end of the class.
-        3. Initial Activity: - Propose an activity that activates students' prior knowledge about ${pergunta}.
-           - Relate the activity to practical situations or fundamentals of the discipline.
-        4. Development: - Divide the content into sections and provide key information about ${pergunta}.
-           - Use visual aids, practical examples, and case studies related to the discipline.
-        5. Practical Activity: - Develop a practical activity that allows students to apply what they have learned.
-           - Include questions that encourage reflection and discussion.
-        6. Group Discussion: - Divide students into groups to discuss different perspectives or approaches related to the specific topic.
-           - Ask each group to share their conclusions with the class.
-        7. Synthesis: - Review the main points covered during the class.
-           - Reinforce key concepts and highlight their relevance in the discipline.
-        8. Assessment Activity: - Create an assessment activity that allows students to demonstrate their understanding of the topic.
-           - Use different formats, such as written questions, oral presentations, or projects.
-        9. Feedback: - Provide constructive feedback on students' performance.
-           - Highlight strengths and areas for improvement.
-        10. Homework: - Assign homework related to the specific topic.
-            - Encourage students to explore additional resources to deepen their understanding.
-        `;
+        plano = `Generate a lesson plan for ${pergunta}. Have an introduction that describes the importance of the topic ${pergunta} in everyday life or in the context of the discipline.
+        Define learning objectives, highlighting what students should understand or be able to do at the end of the lesson.
+        Then provide a starter activity that activates students' prior knowledge about ${pergunta}
+        – relate the activity to practical situations or fundamentals of the discipline.
+        After that, present a development: divide the content into sections and provide key information about ${pergunta}
+        – Use visual resources, practical examples and case studies related to the discipline.
+        Create a hands-on activity that allows students to apply what they have learned, include questions that encourage reflection and discussion.
+        Start a Group Discussion: Divide students into groups to discuss different perspectives or approaches related to the specific topic, asking each group to share their conclusions with the class.
+        Then, summarize the main points covered during the class – reinforce the key concepts and highlight their relevance in the discipline.
+        Moving towards the end, create an assessment activity that allows students to demonstrate their understanding of the topic
+        (use different formats, such as written questions, oral presentations or projects).
+        Finally, provide constructive feedback on students' performance (highlight strengths and areas for improvement) and create homework assignments related to the ${pergunta} to do this, encourage students to explore additional resources to deepen their understanding.`
+        ;
     } else {
         // Plano em português (padrão)
-        plano = `
-        Gere um plano de aula para ${pergunta}.
-        1. Introdução: - Descreva a importância do tema ${pergunta} na vida cotidiana ou no contexto da disciplina.
-        2. Objetivos: - Defina os objetivos de aprendizagem, destacando o que os alunos devem entender ou ser capazes de fazer ao final da aula.
-        3. Atividade Inicial: - Proponha uma atividade que ative o conhecimento prévio dos alunos sobre ${pergunta}.
-         - Relacione a atividade com situações práticas ou fundamentos da disciplina.
-        4. Desenvolvimento: - Divida o conteúdo em seções e forneça informações-chave sobre ${pergunta}. 
-        - Utilize recursos visuais, exemplos práticos e estudos de caso relacionados à disciplina.
-        5. Atividade Prática: - Elabore uma atividade prática que permita aos alunos aplicar o que aprenderam. 
-        - Inclua perguntas que incentivem a reflexão e a discussão.
-        6. Discussão em Grupo: - Divida os alunos em grupos para discutir diferentes perspectivas ou abordagens relacionadas ao tópico específico.
-         - Peça que cada grupo compartilhe suas conclusões com a turma.
-        7. Síntese: - Faça uma revisão dos principais pontos abordados durante a aula. 
-        - Reforce os conceitos-chave e destaque sua relevância na disciplina.
-        8. Atividade de Avaliação: - Crie uma atividade de avaliação que permita aos alunos demonstrar sua compreensão do tópico. 
-        - Utilize diferentes formatos, como questões escritas, apresentações orais ou projetos.
-        9. Feedback: - Forneça feedback construtivo sobre o desempenho dos alunos. 
-        - Destaque pontos fortes e áreas para melhorias.
-        10. Tarefas de Casa: - Atribua tarefas de casa relacionadas ao tópico específico. 
-        - Encoraje os alunos a explorarem recursos adicionais para aprofundar seu entendimento.
-        `;
+        plano = `Gere um plano de aula para ${pergunta}. Disponha de uma introdução  que descreva a importância do tema ${pergunta} no dia a dia ou no contexto da disciplina.
+        Defina os objetivos de aprendizagem, destacando o que os alunos devem entender ou serem capazes de fazer ao final da aula. 
+        Em seguida, forneça uma atividade inicial que ative o conhecimento prévio dos alunos sobre ${pergunta} 
+        – relacione a atividade com situações práticas ou fundamentos da disciplina. 
+        Após isso, apresente um desenvolvimento: divida o conteúdo em seções e forneça informações-chave sobre ${pergunta} 
+        – Utilize recursos visuais, exemplos práticos e estudos de caso relacionados à disciplina. 
+        Crie uma atividade prática que permita aos alunos aplicar o que aprenderam, inclua perguntas que incentivem a reflexão e a discussão. 
+        Inicie uma discussão em Grupo: divida os alunos em grupos para discutir diferentes perspectivas ou abordagens relacionadas  ao tópico específico, pedindo que cada grupo compartilhe suas conclusões com a turma. 
+        Depois, faça uma síntese dos principais pontos abordados durante a aula – reforce os conceitos-chave e destaque sua relevância na disciplina. 
+        Seguindo para o final, elabore uma atividade avaliativa que avaliação que permita aos alunos demonstrar sua compreensão do tópico 
+        (utilize diferentes formatos, como questões escritas, apresentações orais ou projetos). 
+        Por fim forneça um feedback construtivo sobre o desempenho dos alunos (destaque pontos fortes e áreas para melhorias) e crie tarefas de casa  relacionadas  a(o) ${pergunta} para isso, encoraje os alunos a explorarem recursos adicionais para aprofundar seu entendimento.`
+        ;
     }
 
     return plano;
